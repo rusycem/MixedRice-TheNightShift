7d0e466c508dd742d661ead815ca4e793d35a179
joyce-yong <qingacc@gmail.com>
2026-01-31 07:51:54 +0800

    add: idle & run bob

--- Assets/Scripts/Player/HeadBobController.cs
+++ Assets/Scripts/Player/HeadBobController.cs
@@ -1,17 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HeadBobController : MonoBehaviour
 {
     [Header("Configuration")]
     [SerializeField] private bool _enable = true;
-    [SerializeField, Range(0, 0.1f)] private float _Amplitude = 0.015f; [SerializeField, Range(0, 30)] private float _frequency = 10.0f;
-    [SerializeField] private Transform _camera = null; [SerializeField] private Transform _cameraHolder = null;
+    [SerializeField] private Transform _camera = null;
+    [SerializeField] private Transform _cameraHolder = null;
 
-    private float _toggleSpeed = 3.0f;
+    [Header("Speeds")]
+    [SerializeField] private float _walkThreshold = 2.0f;
+    [SerializeField] private float _runThreshold = 7.0f;
+
+    [Header("Idle (Breathing)")]
+    [SerializeField] private float _idleAmp = 0.005f;
+    [SerializeField] private float _idleFreq = 1.5f;
+
+    [Header("Walking")]
+    [SerializeField] private float _walkAmp = 0.02f;
+    [SerializeField] private float _walkFreq = 12.0f;
+
+    [Header("Running")]
+    [SerializeField] private float _runAmp = 0.04f;
+    [SerializeField] private float _runFreq = 16.0f;
+
     private Vector3 _startPos;
     private CharacterController _controller;
+    private float _timer;
 
     private void Awake()
     {
@@ -22,28 +36,49 @@ public class HeadBobController : MonoBehaviour
     void Update()
     {
         if (!_enable) return;
+
         CheckMotion();
         ResetPosition();
         _camera.LookAt(FocusTarget());
     }
-    private Vector3 FootStepMotion()
-    {
-        Vector3 pos = Vector3.zero;
-        pos.y += Mathf.Sin(Time.time * _frequency) * _Amplitude;
-        pos.x += Mathf.Cos(Time.time * _frequency / 2) * _Amplitude * 2;
-        return pos;
-    }
+
     private void CheckMotion()
     {
         float speed = new Vector3(_controller.velocity.x, 0, _controller.velocity.z).magnitude;
-        if (speed < _toggleSpeed) return;
+
         if (!_controller.isGrounded) return;
 
-        PlayMotion(FootStepMotion());
+        if (speed < _walkThreshold)
+        {
+            // Idle
+            PlayBob(_idleFreq, _idleAmp);
+        }
+        else if (speed < _runThreshold)
+        {
+            // Walking
+            PlayBob(_walkFreq, _walkAmp);
+        }
+        else
+        {
+            // Running
+            PlayBob(_runFreq, _runAmp);
+        }
     }
-    private void PlayMotion(Vector3 motion)
+
+    private void PlayBob(float freq, float amp)
     {
-        _camera.localPosition += motion;
+        _timer += Time.deltaTime * freq;
+
+        Vector3 newPos = _camera.localPosition;
+        newPos.y += Mathf.Sin(_timer) * amp;
+        newPos.x += Mathf.Cos(_timer / 2) * amp;
+
+        _camera.localPosition = newPos;
+    }
+
+    private void ResetPosition()
+    {
+        _camera.localPosition = Vector3.Lerp(_camera.localPosition, _startPos, 8.0f * Time.deltaTime);
     }
 
     private Vector3 FocusTarget()
@@ -51,10 +86,5 @@ public class HeadBobController : MonoBehaviour
         Vector3 pos = new Vector3(transform.position.x, transform.position.y + _cameraHolder.localPosition.y, transform.position.z);
         pos += _cameraHolder.forward * 15.0f;
         return pos;
-    }
-    private void ResetPosition()
-    {
-        if (_camera.localPosition == _startPos) return;
-        _camera.localPosition = Vector3.Lerp(_camera.localPosition, _startPos, 1 * Time.deltaTime);
     }
 }
