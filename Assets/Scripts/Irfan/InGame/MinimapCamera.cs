3836d9de630f18ccf0af3be03e9e3aba0aa295ce
M. Irfan Rusydi <67940812+rusycem@users.noreply.github.com>
2026-01-31 22:47:44 +0800

    bobbing is crazy, ui still crying

--- Assets/Scripts/Irfan/InGame/MinimapCamera.cs
+++ Assets/Scripts/Irfan/InGame/MinimapCamera.cs
@@ -3,15 +3,24 @@ using UnityEngine;
 public class MinimapFollow : MonoBehaviour
 {
     public Transform player;
+    public RectTransform playerIconUI; // Drag your UI Arrow here
 
     void LateUpdate()
     {
+        if (player == null) return;
         // follow the player
         Vector3 newPosition = player.position;
         newPosition.y = transform.position.y; 
         transform.position = newPosition;
 
         // always face north
-        //transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+        transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+        float playerRotation = player.eulerAngles.y;
+        if (playerIconUI != null)
+        {
+            // Rotate the UI icon (Z-axis in UI corresponds to Y-axis in 3D)
+            // Use -playerRotation if the icon spins the wrong way
+            playerIconUI.localRotation = Quaternion.Euler(0, 0, -playerRotation);
+        }
     }
 }
