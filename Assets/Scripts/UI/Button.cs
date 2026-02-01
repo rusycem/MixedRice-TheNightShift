665ffe368970f04afab81dcd0a3bd16f8acc2204
joyce-yong <qingacc@gmail.com>
2026-02-01 11:41:48 +0800

    import sfx & add audio on main menu

--- Assets/Scripts/UI/Button.cs
+++ Assets/Scripts/UI/Button.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using System.Collections;
 
-public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     [Header("Animation Settings")]
     public float scaleAmount = 1.15f;
@@ -11,9 +12,13 @@ public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     private Vector3 originalScale;
     private Vector2 originalPosition;
-    
+
     private RectTransform rectTransform;
     private Coroutine activeRoutine;
+
+    [Header("Sound Settings")]
+    public AudioClip hoverClip;
+    public AudioClip clickClip;
 
     void Start()
     {
@@ -26,17 +31,28 @@ public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     public void OnPointerEnter(PointerEventData eventData)
     {
         StopCurrentAnimation();
+
+        // Play hover sound
+        if (hoverClip != null)
+            AudioManager.Instance.PlaySFX(hoverClip);
+
         Vector3 targetScale = originalScale * scaleAmount;
         Vector2 targetPos = originalPosition + new Vector2(-slideAmount, 0f);
-        
+
         activeRoutine = StartCoroutine(AnimateButton(targetScale, targetPos));
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         StopCurrentAnimation();
-        
         activeRoutine = StartCoroutine(AnimateButton(originalScale, originalPosition));
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // Play click sound
+        if (clickClip != null)
+            AudioManager.Instance.PlaySFX(clickClip);
     }
 
     private void StopCurrentAnimation()
@@ -55,12 +71,11 @@ public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         {
             elapsed += Time.deltaTime;
             float t = elapsed / animationDuration;
-            
             float curve = Mathf.SmoothStep(0f, 1f, t);
 
             transform.localScale = Vector3.Lerp(startScale, targetScale, curve);
             rectTransform.anchoredPosition = Vector2.Lerp(startPos, targetPos, curve);
-            
+
             yield return null;
         }
 
